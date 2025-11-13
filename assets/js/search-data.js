a9eb3b565889748532f465efd1ae74c83ad2d558
ltnghia <ltnghia@users.noreply.github.com>
2025-11-13 10:03:46 +0000

    Deploying to gh-pages from @ ltnghia/ltnghia.github.io@5eb586240f6ab25720acf19b144a303bbaf50cf7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-demo",
           title: "Demo",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/demo/";
