683b826f5762443d829c86eb8434304669801337
ltnghia <ltnghia@users.noreply.github.com>
2025-11-13 06:28:05 +0000

    Deploying to gh-pages from @ ltnghia/ltnghia.github.io@2d45abc4fbcd96ad852a981b2daa4fecb9e7b1a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,8 +17,8 @@ ninja.data = [{
             window.location.href = "/blog/";
           },
         },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
