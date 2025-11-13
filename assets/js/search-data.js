f383fa78b05ac0947229355417d6472f44a115f7
ltnghia <ltnghia@users.noreply.github.com>
2025-11-13 06:15:55 +0000

    Deploying to gh-pages from @ ltnghia/ltnghia.github.io@d369dfc366ffec877921543b66d5b3ccbc048155 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-courses",
           title: "Courses",
-          description: "UNIVERSITY OF SCIENCE, VNU-HCM, Vietnam",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
