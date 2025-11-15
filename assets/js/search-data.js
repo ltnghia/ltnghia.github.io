4b34a938230a24e2e985a06c5f6dcf2b12133afb
QDuy0082 <147849106+QDuy0082@users.noreply.github.com>
2025-11-15 15:00:45 +0000

    Deploying to gh-pages from @ ltnghia/ltnghia.github.io@a1b1eae15306b61b8ec2956a31e47da53694deaf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -587,9 +587,9 @@ ninja.data = [{
           description: "Creative Animal Generation from Natural Silhouettes",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-eventa",
+          title: 'EVENTA',
+          description: "Seeing Beyond the Pixels — Understanding the Story Behind Every Image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
